be86e28473d69548f7949a4212a098ce2964e5ca
sebas.programacion <jstobona@uqvirtual.edu.co>
2021-11-22 16:25:01 -0500

    Se corrige lo errores de los paneles de detalle_producto.xhtml y se agrega los comentarios v15.3

--- web/src/main/java/co/edu/uniquindio/proyecto/bean/DetalleProductoBean.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/DetalleProductoBean.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.proyecto.bean;
 
+import co.edu.uniquindio.proyecto.entidades.Comentario;
 import co.edu.uniquindio.proyecto.entidades.Producto;
 import co.edu.uniquindio.proyecto.servicios.ProductoServicio;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.List;
 
 @Component
 @ViewScoped
@@ -25,11 +27,22 @@ public class DetalleProductoBean implements Serializable {
     @Getter @Setter
     private Producto producto;
 
+    @Getter @Setter
+    private Comentario nuevoComentario;
+
+    @Getter @Setter
+    private List<Comentario> comentarios;
+
     @PostConstruct
     public  void  inicializador(){
+        nuevoComentario = new Comentario();
         if (codigoProducto != null && !codigoProducto.isEmpty()) {
             Integer codigo = Integer.parseInt(codigoProducto);
             producto = productoServicio.obtenerProducto(codigo);
         }
+    }
+
+    public void crearComentario(){
+
     }
 }
